ab4f3cafa5c1c0a2a25556c35354bf8615e7953a
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2025-10-28 17:25:21 +0400

    fix: bigger gas limit for deployment

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -15,7 +15,9 @@ dotenvConfig();
 
 task("deploy").addParam("shoyubashi", "The address of the ShoyuBashi oracle").setAction(async (args, hre) => {
   const Accounting = await hre.ethers.getContractFactory("Accounting");
-  const accounting = await Accounting.deploy(args.shoyubashi);
+  const accounting = await Accounting.deploy(args.shoyubashi, {
+    gasLimit: 10000000
+  });
   const accountingAddr = await accounting.waitForDeployment();
 
   console.log(`Accounting address: ${accountingAddr.target}`);
