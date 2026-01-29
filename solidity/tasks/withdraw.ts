4886bfead64bb0124a2004e9a255be14b672773c
ptrus <peter@u-s.si>
2026-01-29 15:21:28 +0200

    restructure hardhat tasks

--- solidity/tasks/withdraw.ts
+++ solidity/tasks/withdraw.ts
@@ -272,23 +272,3 @@ task("watchWithdrawal")
     return { resolved: false };
   });
 
-task("setGasPrice")
-  .addParam("contract", "Accounting contract address")
-  .addParam("chainid", "Chain ID")
-  .addParam("gasprice", "Gas price in wei")
-  .setAction(async (args, hre) => {
-    const accounting = await hre.ethers.getContractAt(
-      "Accounting",
-      args.contract,
-    );
-    console.log(
-      "Setting gas price for chain",
-      args.chainid,
-      "to",
-      args.gasprice,
-    );
-    const tx = await accounting.setGasPrice(args.chainid, args.gasprice);
-    console.log("Transaction:", tx.hash);
-    await tx.wait();
-    console.log("Done!");
-  });
